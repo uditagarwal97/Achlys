f10001315dc39c7c2bc5812d4721cdf9b409add0
uditagarwal97 <uditagarwal1997@gmail.com>
2022-12-03 21:52:37 -0800

    Update Matt benchmark

--- Benchmarks/underhanded-c/matt_bierner.c
+++ Benchmarks/underhanded-c/matt_bierner.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <math.h>
 #include <fenv.h>
 #include <float.h>
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -12,41 +12,39 @@
 */
 #define ERROR_LABEL "\x89\xec\x5d\xc2\0\0 - "
 
-static void* error_message = ERROR_LABEL "A math error occured. Returning that no match was found.";
+static char *error_message =
+    ERROR_LABEL "A math error occured. Returning that no match was found.";
 
-//typedef void (*f)() error_handler;
+void error_messager() { printf("%s", error_message); }
 
-void error_messager() {
-    printf("%s", error_message);
-}
-
-int do_elements_match(double sample1, double sample2, double threshold, void (*on_error)()) {
-    double sqroot = sqrt(sample1 - sample2);
-    // Check to see if a math error occured.
-    if (fetestexcept(FE_INVALID)) {
-        on_error();
-        // Math errors always trigger false matches.
-        return 0;
-    }
-    return sqroot <= threshold;
-}
-
-int match(double* test, double* reference, int bins, double threshold) {
-    for (unsigned i = 0; i < bins; ++i)
-        if (!do_elements_match(test[i], reference[i], threshold, error_message))
-            return 0;
-    return 1;
-}
-
-int main(int argc, char** argv) {
-//    double test[] = {90, 10, 2, 8, 7};
-    double test[5];
-    double reference[] = {100, 100, 100, 100, 100};
-    for (int i = 0; i < 5; i++) {
-        test[i] = atof(argv[i+1]);
-    }
-    int bins = 5;
-    int threshold = 10;
-    match(test, reference, bins, threshold);
+int do_elements_match(double sample1, double sample2, double threshold,
+                      void (*on_error)()) {
+  double sqroot = sqrt(sample1 - sample2);
+  // Check to see if a math error occured.
+  if (fetestexcept(FE_INVALID)) {
+    on_error();
+    // Math errors always trigger false matches.
     return 0;
+  }
+  return sqroot <= threshold;
+}
+
+int match(double *test, double *reference, int bins, double threshold) {
+  for (unsigned i = 0; i < bins; ++i)
+    if (!do_elements_match(test[i], reference[i], threshold, &error_messager))
+      return 0;
+  return 1;
+}
+
+int main(int argc, char **argv) {
+  //    double test[] = {90, 10, 2, 8, 7};
+  double test[5];
+  double reference[] = {100, 100, 100, 100, 100};
+  for (int i = 0; i < 5; i++) {
+    test[i] = atof(argv[i + 1]);
+  }
+  int bins = 5;
+  int threshold = 10;
+  match(test, reference, bins, threshold);
+  return 0;
 }

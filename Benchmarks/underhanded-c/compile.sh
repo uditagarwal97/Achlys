03f59831feff4c5e17b1cf065c83b7af73403593
uditagarwal97 <uditagarwal1997@gmail.com>
2022-12-03 16:00:22 -0800

    Add a compilation script for underhanded-c benchmarks

---
+++ Benchmarks/underhanded-c/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CFLAGS="-O0 -Xclang -disable-O0-optnone"
+OPTFLAGS="-O0 -licm -argpromotion -deadargelim -adce -always-inline -inline -mem2reg -mergereturn -dse"
+
+declare -a testCases=("stephen_dolan" "phillip_klenze" "peter_eastman"
+                      "michael_dunphy" "matt_bierner" "linus_akesson_2"
+                      "linus_akesson_1" "josh_lospinoso" "ghislain_lemaur")
+
+for str in "${testCases[@]}"
+do
+  $ACHLYSLLVM/clang++ -emit-llvm -S ${str}.c ${CFLAGS} -o ${str}.ll
+  $ACHLYSLLVM/opt -enable-new-pm=0 ${OPTFLAGS} -S ${str}.ll -o ${str}.ll
+done
